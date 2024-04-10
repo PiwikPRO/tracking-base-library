565680a9ad2fdad06f98a94cc63d6132ba880826
Michał Warać <49625375+auto200@users.noreply.github.com>
2024-04-10 14:47:48 +0200

    Update deps (#9)
    * chore: update deps
    
    * refactor: configure typedoc to target master branch in source links

--- scripts/postProcessDocs.js
+++ scripts/postProcessDocs.js
@@ -2,8 +2,6 @@ import { readFileSync, writeFileSync } from 'node:fs'
 
 const README = 'README.md'
 
-const BLOB_REGEX = /\/blob\/([^\/]+)\//
-
 const file = readFileSync(README, 'utf-8')
 
 const formattedOutput = file
@@ -16,8 +14,6 @@ const formattedOutput = file
   .filter((line) => !line.includes('### Functions'))
   // remove remove additional prefix
   .map((line) => line.replace('Namespace: ', ''))
-  // replace branch specific source code reference with master branch
-  .map((line) => line.replace(BLOB_REGEX, '/blob/master/'))
   .join('\n')
 
 writeFileSync(README, formattedOutput)
