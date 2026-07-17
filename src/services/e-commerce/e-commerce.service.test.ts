63c4b707d3cd10eda00cc970a96be0658793c5b5
Paweł Fajfer <p.fajfer@piwik.pro>
2026-07-17 12:18:13 +0200

    test: added integration and ecommerce unit tests

--- src/services/e-commerce/e-commerce.service.test.ts
+++ src/services/e-commerce/e-commerce.service.test.ts
@@ -1,4 +1,7 @@
-import { ECOMMERCE_V2_TRACK_EVENT } from '../../constants/track-event.constant'
+import {
+  ECOMMERCE_TRACK_EVENT,
+  ECOMMERCE_V2_TRACK_EVENT,
+} from '../../constants/track-event.constant'
 import { PaymentInformation } from '../../interfaces/payment'
 import { Product } from '../../interfaces/product'
 import { EcommerceOptions } from '../../interfaces/utils'
@@ -80,6 +83,91 @@ describe('eCommerce V2', () => {
       products,
       payment,
       options,
+    ])
+  })
+})
+
+describe('eCommerce V1 (deprecated)', () => {
+  it('tracks adding a single ecommerce item', () => {
+    eCommerce.addEcommerceItem('SKU-1', 'Product 1', 'Category', 9.99, 2)
+
+    expectPaqEvent([
+      ECOMMERCE_TRACK_EVENT.ADD_ECOMMERCE_ITEM,
+      'SKU-1',
+      'Product 1',
+      'Category',
+      9.99,
+      2,
+    ])
+  })
+
+  it('tracks removing a single ecommerce item by SKU', () => {
+    eCommerce.removeEcommerceItem('SKU-1')
+
+    expectPaqEvent([ECOMMERCE_TRACK_EVENT.REMOVE_ECOMMERCE_ITEM, 'SKU-1'])
+  })
+
+  it('tracks an ecommerce order with all totals', () => {
+    eCommerce.trackEcommerceOrder('order-1', 100, 80, 10, 5, 5)
+
+    expectPaqEvent([
+      ECOMMERCE_TRACK_EVENT.TRACK_ECOMMERCE_ORDER,
+      'order-1',
+      100,
+      80,
+      10,
+      5,
+      5,
+    ])
+  })
+
+  it('tracks an ecommerce order with only the required totals', () => {
+    eCommerce.trackEcommerceOrder('order-1', 100)
+
+    expectPaqEvent([
+      ECOMMERCE_TRACK_EVENT.TRACK_ECOMMERCE_ORDER,
+      'order-1',
+      100,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+    ])
+  })
+
+  it('tracks an ecommerce cart update', () => {
+    eCommerce.trackEcommerceCartUpdate(19.98)
+
+    expectPaqEvent([ECOMMERCE_TRACK_EVENT.TRACK_ECOMMERCE_CART_UPDATE, 19.98])
+  })
+
+  it('clears the ecommerce cart', () => {
+    eCommerce.clearEcommerceCart()
+
+    expectPaqEvent([ECOMMERCE_TRACK_EVENT.CLEAR_ECOMMERCE_CART])
+  })
+
+  it('tracks an ecommerce product view with all details', () => {
+    eCommerce.setEcommerceView('SKU-1', 'Product 1', ['Category'], '9.99')
+
+    expectPaqEvent([
+      ECOMMERCE_TRACK_EVENT.SET_ECOMMERCE_VIEW,
+      'SKU-1',
+      'Product 1',
+      ['Category'],
+      '9.99',
+    ])
+  })
+
+  it('tracks an ecommerce product view with only a SKU', () => {
+    eCommerce.setEcommerceView('SKU-1')
+
+    expectPaqEvent([
+      ECOMMERCE_TRACK_EVENT.SET_ECOMMERCE_VIEW,
+      'SKU-1',
+      undefined,
+      undefined,
+      undefined,
     ])
   })
 })
